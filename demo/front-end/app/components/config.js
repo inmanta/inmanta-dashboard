5b2f145937ea157677f826bf32a306ab17bbd2d9
Wouter De Borger <wouter@deborger.eu>
2015-10-26 11:24:45 +0100

    changed default server to localhost

--- demo/front-end/app/components/config.js
+++ demo/front-end/app/components/config.js
@@ -1,5 +1,7 @@
 angular.module('imperaApi.config',[])
 .constant('imperaConfig', {
-  'backend': 'http://172.17.4.31:8888/'
+  'backend': 'http://172.0.0.1:8888/'
+//  'backend': 'http://172.17.4.31:8888/'
 //  'backend': 'http://134.58.46.90:8888/'
+//    'backend':'http://verdana.cs.kuleuven.be:8888/'
 })
