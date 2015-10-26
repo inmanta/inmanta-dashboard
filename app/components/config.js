15bebe958a202ac1a73769737edbe61aec1c06d3
Wouter De Borger <wouter@deborger.eu>
2015-10-26 16:21:42 +0100

    big move

--- app/components/config.js
+++ app/components/config.js
@@ -2,6 +2,7 @@ angular.module('imperaApi.config',[])
 .constant('imperaConfig', {
   'backend': 'http://172.0.0.1:8888/'
 //  'backend': 'http://172.17.4.31:8888/'
+//  'backend': 'http://192.168.104.138:8888/'
 //  'backend': 'http://134.58.46.90:8888/'
 //    'backend':'http://verdana.cs.kuleuven.be:8888/'
 })
