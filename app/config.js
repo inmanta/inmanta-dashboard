bf05207f229f0c934f329fef681f862a35851c0c
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2016-06-22 15:34:24 +0200

    Dynamically set the correct host and port

--- app/config.js
+++ app/config.js
@@ -1,10 +1,1 @@
-angular.module('imperaApi.config',[])
-.constant('imperaConfig', {
-  'backend': 'http://127.0.0.1:8888/'
- // 'backend': 'http://172.17.4.31:8888/'
-//  'backend': 'http://192.168.104.138:8888/'
-//  'backend': 'http://134.58.46.90:8888/'
-//    'backend':'http://verdana.cs.kuleuven.be:8888/'
-//    'backend':'http://'+window.location.hostname+':8888/'
-//        'backend':'http://172.17.3.66:8888/'
-})
+angular.module('imperaApi.config',[]).constant('imperaConfig', {'backend':'http://'+window.location.host+'/'})
