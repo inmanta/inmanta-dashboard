f1a365451a52daaaedb22c17446de4d1a3f7a98a
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2016-09-09 08:56:15 +0200

    Use window origin

--- app/config.js
+++ app/config.js
@@ -1,1 +1,1 @@
-angular.module('imperaApi.config',[]).constant('imperaConfig', {'backend':'http://127.0.0.1:8888/'})
+angular.module('imperaApi.config',[]).constant('imperaConfig', {'backend':window.location.origin+'/'})
