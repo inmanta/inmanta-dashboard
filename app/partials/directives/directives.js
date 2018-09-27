ae94bf557e2294de79254bbe654aa680f05f6376
Wouter De Borger <wouter.deborger@inmanta.com>
2018-09-27 15:46:07 +0200

    added new state skipped_for_undefined

--- app/partials/directives/directives.js
+++ app/partials/directives/directives.js
@@ -12,7 +12,7 @@ inmantaApi.filter('nozero', function() {
 });
 
 inmantaApi.directive("deployProgress", function() {
-    var typesSeq = ['failed',  'undefined', 'skipped', 'deployed', 'unavailable', 'cancelled',];
+    var typesSeq = ['failed',  'undefined', 'skipped', 'deployed', 'unavailable', 'cancelled', 'skipped_for_undefined'];
     var types = {
         'deployed': 'success',
         'skipped': 'info',
