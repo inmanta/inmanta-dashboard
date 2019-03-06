469d0d68fc2993839d128051def850b51cc76896
Wouter De Borger <wouter.deborger@inmanta.com>
2019-03-06 10:44:38 +0100

    made all none-nreaking changes

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 
 angular.module('inmantaApi.config', []).constant('inmantaConfig', {
-    'backend': 'http://10.0.254.1:8888/',
+    'backend': 'http://127.0.0.1:8888/',
     'lcm': 'http://10.0.254.1:8889/'
 
 });
