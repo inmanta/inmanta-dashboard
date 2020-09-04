125997c574571cde0423bf12c216a8940a515c51
Andras Kovari <andras.kovari@inmanta.com>
2020-09-04 15:31:26 +0200

    Add big red button

--- app/partials/sideController.js
+++ app/partials/sideController.js
@@ -2,11 +2,23 @@
 
 var resv = angular.module('InmantaApp.controllers.side', ['ui.router'])
 
-resv.controller('sideController', ['$scope', '$rootScope', 'inmantaService', 'inmantaConfig', "$stateParams", function ($scope, $rootScope, inmantaService, inmantaConfig, $stateParams) {
+resv.controller('sideController', ['$scope', '$rootScope', 'inmantaService', 'inmantaConfig', "$stateParams", "dialogs", function ($scope, $rootScope, inmantaService, inmantaConfig, $stateParams, dialogs) {
 	$scope.state = $stateParams;
-	inmantaService.getConsoleAvailable().then(function(lsm) {
+	inmantaService.getConsoleAvailable().then(function (lsm) {
 		$scope.lsm = true;
-	}).catch(function(rejection) { 
+	}).catch(function (rejection) {
 		$scope.lsm = false;
 	});
+	inmantaService.getEnvironment($stateParams.env).then(function (d) {
+		$scope.state.halted = d.halted;
+	});
+	$scope.confirmHalt = function () {
+		var dlg = dialogs.confirm("Halt all operations", "Do you really want to halt all operations in environment " + $stateParams.env + "?");
+		dlg.result.then(function (btn) {
+			inmantaService.haltEnvironment($stateParams.env).then(function (d) { $rootScope.$broadcast('refresh'); $scope.state.halted = true; });
+		});
+	};
+	$scope.resumeEnv = function () {
+		inmantaService.resumeEnvironment($stateParams.env).then(function (d) { $rootScope.$broadcast('refresh'); $scope.state.halted = false; });
+	}
 }]);
